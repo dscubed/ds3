3fb6a032e0a587c2e1cfaa2a48ebb38e29214123
Michael Ren <michaelren0129@gmail.com>
2024-01-03 22:22:47 +0800

    Improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/app/components/Navbar"
 import Matrix from "@/app/components/matrix"
 import { useEffect } from "react"
 import { mapToRange } from '@/app/lib/utils'
+import { watchTimeout, watchInterval } from '@/app/lib/performance'
 
 class Trail {
   constructor (x, y, distance) {
@@ -22,49 +23,57 @@ class Trail {
   }
 }
 
+function setupShimmer (mtx) {
+  mtx.pixels.forEach(pixel => {
+    pixel.onStateChange(() => {
+      // Show pixel if it is set to 'on' state
+      // unless it is hidden by on-scroll animation
+      if (pixel.state === 1 && pixel.isFaded && !pixel.isHidden) {
+        pixel.domNode.style.opacity = '1'
+      }
+    })
+
+    const fadeOutId = setInterval(() => {
+      if (pixel.state === 0 && !pixel.isFaded && !pixel.isHidden) {
+        pixel.isFaded = true
+        pixel.domNode.style.opacity = '0.5'
+      }
+    }, 1 + Math.random() * 1000 * 10)
+
+    const fadeInId = setInterval(() => {
+      if (pixel.state === 0 && pixel.isFaded && !pixel.isHidden) {
+        pixel.isFaded = false
+        pixel.domNode.style.opacity = '1'
+      }
+    }, 5 + Math.random() * 1000 * 10)
+
+    // Remove intervals when pixel is deleted
+    const _delete = pixel.delete.bind(pixel)
+    pixel.delete = () => {
+      clearInterval(fadeInId)
+      clearInterval(fadeOutId)
+      _delete()
+    }
+  })
+}
+
 export default function Index() {
   useEffect(() => {
+    watchInterval()
+    watchTimeout()
+
     const matrix = new Matrix('#matrix')
 
-    matrix.setup(mtx => {
-      mtx.trails = []
+    function mouseMoveCallback (event) {
+      const distance = Math.sqrt(event.movementX**2 + event.movementY**2)
+      this.trails.push(new Trail(this.mouseX, this.mouseY, distance))
+    }
 
-      mtx.pixels.forEach(pixel => {
-        pixel.onStateChange(() => {
-          // Show pixel if it is set to 'on' state
-          // unless it is hidden by on-scroll animation
-          if (pixel.state === 1 && pixel.isFaded && !pixel.isHidden) {
-            pixel.domNode.style.opacity = '1'
-          }
-        })
+    var scrollDelta = 0
+    var prevScollY = 0
+    var lastScrollAt = 0
 
-        const fadeOutId = setInterval(() => {
-          if (pixel.state === 0 && !pixel.isFaded && !pixel.isHidden) {
-            pixel.isFaded = true
-            pixel.domNode.style.opacity = '0.5'
-          }
-        }, 1 + Math.random() * 1000 * 10)
-
-        const fadeInId = setInterval(() => {
-          if (pixel.state === 0 && pixel.isFaded && !pixel.isHidden) {
-            pixel.isFaded = false
-            pixel.domNode.style.opacity = '1'
-          }
-        }, 5 + Math.random() * 1000 * 10)
-
-        const _delete = pixel.delete.bind(pixel)
-        pixel.delete = () => {
-          _delete()
-          clearInterval(fadeInId)
-          clearInterval(fadeOutId)
-        }
-      })
-
-      window.addEventListener('mousemove', event => {
-        const distance = Math.sqrt(event.movementX**2 + event.movementY**2)
-        mtx.trails.push(new Trail(mtx.mouseX, mtx.mouseY, distance))
-      })
-      
+    function scrollCallback (event) {
       /* 
         How the on scroll animation works:
         1. A pixel is eligible to undergo animation transition if it's near the top of the screen/window
@@ -73,83 +82,93 @@ export default function Index() {
         4. A random number is generated, and if the probability condition is met, the pixel will transition
         5. If the probability condition failed, step 1 to 4 will be repeated in the next event cycle
       */
+    
+      scrollDelta = window.scrollY - prevScollY
 
-      var scrollDelta = 0
-      var prevScollY = 0
-      var lastScrollAt = 0
+      // ScrollDelta never be 0 inside the event listener callback, return to be safe
+      if (scrollDelta === 0) {
+        return
+      }
 
-      window.addEventListener('scroll', event => {
-        scrollDelta = window.scrollY - prevScollY
+      const isScrollTop = scrollDelta < 0 && window.scrollY === 0 // only true of scroll direction is up
 
-        // ScrollDelta never be 0 inside the event listener callback, return to be safe
-        if (scrollDelta === 0) {
-          return
-        }
+      // To save resources, only run the animation cycle every 25ms, or 40 times per second
+      // However, we need to override debounce when the user has just scrolled to the top
+      // to transition the rest of the pixels near the top of the screen.
+      if(Date.now() - lastScrollAt > 25 || isScrollTop) { // Debounce -> run action every 25ms
+        // Find where the window top intersect the grid
+        const [, gridY] = this.screenToGridPos(0, window.scrollY)
 
-        const isScrollTop = scrollDelta < 0 && window.scrollY === 0 // only true of scroll direction is up
+        // We want to fade pixels when scrolling down and show pixels when up
+        const isHiddenFilter = prevScollY < window.scrollY ? false : true
 
-        // To save resources, only run the animation cycle every 25ms, or 40 times per second
-        // However, we need to override debounce when the user has just scrolled to the top
-        // to transition the rest of the pixels near the top of the screen.
-        if(Date.now() - lastScrollAt > 25 || isScrollTop) { // Debounce -> run action every 25ms
-          // Find where the window top intersect the grid
-          const [, gridY] = mtx.screenToGridPos(0, window.scrollY)
+        this.pixels.forEach(pixel => {
+          const margin = Math.round(Math.random() * 3)
+          const isNearWindowTop = isScrollTop 
+            ? true 
+            : scrollDelta > 0 
+              ? pixel.y < gridY + margin 
+              : pixel.y > gridY + margin
 
-          // We want to fade pixels when scrolling down and show pixels when up
-          const isHiddenFilter = prevScollY < window.scrollY ? false : true
+          if (pixel.isHidden === undefined) {
+            pixel.isHidden = false
+          }
 
-          mtx.pixels.forEach(pixel => {
-            const margin = Math.round(Math.random() * 3)
-            const isNearWindowTop = isScrollTop 
-              ? true 
-              : scrollDelta > 0 
-                ? pixel.y < gridY + margin 
-                : pixel.y > gridY + margin
-
-            if (pixel.isHidden === undefined) {
-              pixel.isHidden = false
+          if (pixel.isHidden === isHiddenFilter && isNearWindowTop) {
+            // Skip if pixel already has a ongoing time delay
+            // UNLESS the user has scrolled to the top because there is no next event callback
+            if (pixel.pendingTimeout && !isScrollTop) {
+              return
             }
 
-            if (pixel.isHidden === isHiddenFilter && isNearWindowTop) {
-              // Skip if pixel already has a ongoing time delay
-              // UNLESS the user has scrolled to the top because there is no next event callback
-              if (pixel.pendingTimeout && !isScrollTop) {
-                return
+            pixel.pendingTimeout = true
+
+            setTimeout(() => {
+              // Pixels closer to the top of the screen will be more likely to undergo transition
+              // mapToRange(<pixel's distance to top>, 0, <max margin>, <minimum chance>, <maximum chance>)
+              let likelihood = mapToRange(pixel.y - gridY, 0, 3, 0.25, 1)
+  
+              // Must transition if pixel is off screen; or the user has scrolled to the top
+              // and we need to show all pixel now (because there is no next event callback)
+              if (pixel.y - gridY < 0 || (isScrollTop)) {
+                likelihood = 1
               }
+  
+              if (Math.random() < likelihood) {
+                pixel.domNode.style.opacity = scrollDelta > 0 
+                  ? '0' 
+                  : ''
 
-              pixel.pendingTimeout = true
+                pixel.isHidden = scrollDelta > 0 
+                  ? true
+                  : false
+              }
+              
+              pixel.pendingTimeout = false
+            }, Math.random() * 250)
+          }
+        })
 
-              setTimeout(() => {
-                // Pixels closer to the top of the screen will be more likely to undergo transition
-                // mapToRange(<pixel's distance to top>, 0, <max margin>, <minimum chance>, <maximum chance>)
-                let likelihood = mapToRange(pixel.y - gridY, 0, 3, 0.25, 1)
-    
-                // Must transition if pixel is off screen; or the user has scrolled to the top
-                // and we need to show all pixel now (because there is no next event callback)
-                if (pixel.y - gridY < 0 || (isScrollTop)) {
-                  likelihood = 1
-                }
-    
-                if (Math.random() < likelihood) {
-                  pixel.domNode.style.opacity = scrollDelta > 0 
-                    ? '0' 
-                    : ''
+        lastScrollAt = Date.now()
+      }
 
-                  pixel.isHidden = scrollDelta > 0 
-                    ? true
-                    : false
-                }
-                
-                pixel.pendingTimeout = false
-              }, Math.random() * 250)
-            }
-          })
+      prevScollY = window.scrollY
+    }
 
-          lastScrollAt = Date.now()
-        }
+    matrix.setup(mtx => {
+      mtx.trails = []
+      mtx.mouseMoveCallback = mouseMoveCallback.bind(mtx)
+      mtx.scrollCallback = scrollCallback.bind(mtx)
+      window.addEventListener('mousemove', mtx.mouseMoveCallback)
+      window.addEventListener('scroll', mtx.scrollCallback)
+      setupShimmer(mtx)
+    })
 
-        prevScollY = window.scrollY
-      })
+    matrix.onResize(mtx => {
+      mtx.trails = []
+      window.addEventListener('mousemove', mtx.mouseMoveCallback)
+      window.addEventListener('scroll', mtx.scrollCallback)
+      setupShimmer(mtx)
     })
 
     matrix.update(mtx => {
