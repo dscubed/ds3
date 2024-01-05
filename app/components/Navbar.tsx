be433f21b30d1133e720d19d49945544df6549ee
Michael Ren <michaelren0129@gmail.com>
2024-01-05 23:43:15 +0800

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,27 +14,27 @@ export default function Navbar ({
     <nav
       {...rest}
       className={clsx(
-        'sticky top-0 w-full px-4 py-2 bg-background-secondary',
+        'sticky top-0 w-full px-4 py-3 bg-background-secondary z-20',
         className
       )}
     >
       <div className="max-w-screen-xl flex justify-between gap-3 mx-auto">
         {/* saturate-0 brightness-[10] */}
         <div className="flex flex-1">
-          {/* <h3 className="font-medium my-auto">DSCubed</h3> */}
-          <Image
-            className="filter-invert"
-            src='/logo.png' 
-            width={1363/15} 
-            height={552/15} 
-            alt="DS Cubed logo"
-          ></Image>
+          <Link href="/">
+            <Image
+              className="filter-invert"
+              src='/logo.png' 
+              width={1363/14} 
+              height={552/14} 
+              alt="DS Cubed logo"
+            ></Image>
+          </Link>
         </div>
         <div className="flex gap-6">
-          <Link className="my-auto" href="#">Committee</Link>
-          <Link className="my-auto" href="#">Sponsors</Link>
+          <Link className="my-auto" href="committee">Committee</Link>
+          <Link className="my-auto" href="sponsers">Sponsors</Link>
           <Link className="my-auto" href="#">Events</Link>
-          <Link className="my-auto" href="#">Projects</Link>
           <Link className="my-auto" href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/dscubed/" target="_blank">Membership</Link>
         </div>
       </div>
