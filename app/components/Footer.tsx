edcec25c8b8113f895a45ee6750dbe2cf1aa0f72
Michael Ren <michaelren0129@gmail.com>
2024-01-05 14:32:21 +0800

    Add footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import * as Icon from 'react-bootstrap-icons';
+
+export default function Footer () {
+  return (
+    <footer>
+      <div className="flex flex-col gap-4 max-w-screen-xl mx-auto">
+        <div className="flex gap-4 justify-between py-8 text-text-secondary">
+          <p className="">© {(new Date).getFullYear()} DS Cubed</p>
+          <div className="flex gap-8">
+            <Link href="mailto:hello@dscubed.au" target="_blank">
+              <Icon.EnvelopeFill className="w-5 h-5"/>
+            </Link>
+            <Link href="https://instagram.com/dscubed.unimelb" target="_blank">
+              <Icon.Instagram className="w-5 h-5"/>
+            </Link>
+            <Link href="https://www.facebook.com/dscubed.unimelb/" target="_blank">
+              <Icon.Facebook className="w-5 h-5"/>
+            </Link>
+            <Link href="https://discord.gg/hFX4PJZ5MQ" target="_blank">
+              <Icon.Discord className="w-5 h-5"/>
+            </Link>
+            <Link href="https://medium.com/dscubed-unimelb" target="_blank">
+              <Icon.Medium className="w-5 h-5"/>
+            </Link>
+            <Link href="https://www.linkedin.com/company/data-science-student-society/" target="_blank">
+              <Icon.Linkedin className="w-5 h-5"/>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
