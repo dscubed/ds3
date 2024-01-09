c0cec07b672042b07cf1b842401738014a0dfe42
Michael Ren <michaelren0129@gmail.com>
2024-01-09 11:11:21 +0800

    Bug fix

--- app/components/FAQItem.tsx
+++ app/components/FAQItem.tsx
@@ -14,14 +14,13 @@ export default function FAQItem ({
 
   return (
     <div
-      className="flex flex-col border-b border-border py-8 transition-all cursor-pointer"
+      className="flex flex-col border-b border-border py-8 transition-all cursor-pointer overflow-hidden"
       onClick={e => toggleAnswer(showAnswer ? false : true)}
     >
       <div className="grid grid-cols-[max-content,1fr] gap-x-4">
         <PlusIcon className={`w-8 h-8 text-text-secondary transition-all ${showAnswer ? 'rotate-45' : 'rotate-0'}`} />
-        <h4 className="text-xl my-auto">{question}</h4>
-        {/* <div></div> */}
-        <p className={`transition-all duration-300 text-xl text-text-secondary col-start-2 ${showAnswer ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
+        <h4 className="text-xl leading-tight my-auto">{question}</h4>
+        <p className={`transition-all duration-300 text-xl text-text-secondary leading-relaxed col-start-2 ${showAnswer ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
           <span className="block pt-4">{answer}</span>
         </p>
       </div>
