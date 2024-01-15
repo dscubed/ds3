ce6568990425bc99309f3d2266f3b9c13fbbe95a
Michael Ren <michaelren0129@gmail.com>
2024-01-15 19:24:14 +0800

    Update SponserCard.tsx

--- app/components/sponsors/SponserCard.tsx
+++ app/components/sponsors/SponserCard.tsx
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,34 +9,33 @@ export default function SponsorCard ({
   color,
   image,
   link,
-  invert = false,
+  filter = '',
 }: {
   name: String,
   category: String,
   color: String,
   image: String,
   link: String,
-  invert: Boolean
+  filter: String
 }) {
   return (
     <div className='relative h-[400px] sm:h-[300px] rounded-md overflow-hidden' style={{backgroundColor: color}}>
       <div className="absolute w-full grid grid-cols-[1fr,auto] p-4">
         <div className="flex flex-col gap-2">
-          <h5 className="text-xl leading-none text-white">{name}</h5>
-          <p className="text-white text-opacity-40 leading-none text-lg">{category}</p>
+          <h5 className="text-xl leading-none text-text-primary">{name}</h5>
+          <p className="text-black/40 dark:text-white/40 leading-none text-lg">{category}</p>
         </div>
         <Link
           href={link} 
           target="_blank"
-          className="flex bg-white bg-opacity-10 text-white text-opacity-50 hover:text-opacity-100 transition text-background gap-2 px-4 py-1.5 rounded-md cursor-pointer"
+          className="flex bg-black/10 dark:bg-white/10 text-black/50 dark:text-white/50 hover:text-opacity-100 transition gap-2 px-4 py-1.5 rounded-md cursor-pointer"
         >
           <span className="my-auto w-max">More</span>
           <ArrowRightIcon className="my-auto w-5 h-5"/>
         </Link>
       </div>
       <Image
-        className="absolute w-80 sm:w-60 h-max max-h-16 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] saturate-0 contrast-125 brightness-[10]"
-        style={{filter: invert ? 'invert(1)' : ''}}
+        className={clsx("absolute w-80 sm:w-60 h-max max-h-16 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] saturate-0 object-contain dark:invert pointer-events-none", filter)}
         src={image}
         width={80*4}
         height={40}
