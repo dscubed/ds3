9bcfc8513fb3ab57a7b026ebcad782c1691def91
Michael Ren <michaelren0129@gmail.com>
2024-01-16 15:22:05 +0800

    Only allow /map page in dev

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import dynamic from 'next/dynamic'
+import ErrorPage from 'next/error'
 
 const P5Map = dynamic(
   () => import('@/app/components/P5Map'),
@@ -6,6 +8,12 @@ const P5Map = dynamic(
 )
 
 export default function Map () {
+  // It is CRUCIAL that this page is only accessible in dev mode to prevent unwanted charges
+  // from Google Static Map API
+  if (process.env.NODE_ENV === 'production') {
+    return <ErrorPage statusCode={404} />
+  }
+
   return (
     <P5Map />
   )
