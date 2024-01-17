3d8f9ccac81afe8e5d8a55ac4e4d2289d0fa50e7
Michael Ren <michaelren0129@gmail.com>
2024-01-17 23:05:07 +0800

    Improve login page UI

--- app/components/admin/login/GoogleAuthButton.tsx
+++ app/components/admin/login/GoogleAuthButton.tsx
@@ -1,13 +1,11 @@
 'use client'
 import { Google } from "react-bootstrap-icons";
 import { signIn } from "@/app/lib/auth.client";
+import clsx from "clsx";
 
-export default function GoogleAuthButton () {
+export default function GoogleAuthButton ({ className = '' }: { className?: string }) {
   return (
-    <button 
-      onClick={signIn}
-      className="flex gap-4 px-6 py-4 text-xl bg-background border border-border rounded-full"
-    >
+    <button onClick={signIn} className={clsx("flex w-max gap-4 px-4 py-3 text-xl bg-background border border-border rounded-full", className)} >
       <Google className="w-6 h-6 my-auto" />
       <span className="my-auto">Login with Google</span>
     </button>
