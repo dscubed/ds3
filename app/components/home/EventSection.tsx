1ba5ba5cb45f320852b377db5c589ba9bfafc270
Michael Ren <michaelren0129@gmail.com>
2024-01-21 00:25:11 +0800

    Add event suspense on home page

--- app/components/home/EventSection.tsx
+++ app/components/home/EventSection.tsx
@@ -2,13 +2,17 @@ import Link from 'next/link'
 import Section from '@/app/components/Section'
 import EventGallery from '@/app/components/events/EventGallery'
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import { Suspense } from 'react'
+import EventGallerySkeleton from '../events/EventGallerySkeleton'
 
 export default function EventSection () {
   return (
     <Section>
       <h2 className="text-4xl text-center mx-auto mb-5 leading-tight">Recent updates</h2>
 
-      <EventGallery range={[0, 3]} />
+      <Suspense fallback={<EventGallerySkeleton />}>
+        <EventGallery range={[0, 3]} />
+      </Suspense>
       
       <Link className="flex gap-2 text-xl text-theme mx-auto" href="/events">
         <span className="my-auto">Browse All Events</span>
