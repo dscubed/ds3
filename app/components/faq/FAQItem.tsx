746e6ca14693d7ad0bd09ceb0c58142a50a1953e
Michael Ren <michaelren0129@gmail.com>
2024-02-01 14:34:45 +0800

    Update FAQItem.tsx

--- app/components/faq/FAQItem.tsx
+++ app/components/faq/FAQItem.tsx
@@ -19,9 +19,9 @@ export default function FAQItem ({
     >
       <div className="grid grid-cols-[max-content,1fr] gap-x-4">
         {/* Set icon height to line height */}
-        <PlusIcon className={`w-7 h-[calc(1.25rem*1.625)] text-text-secondary transition-all ${showAnswer ? 'rotate-45' : 'rotate-0'}`} />
-        <h3 className="text-xl leading-relaxed my-auto">{question}</h3>
-        <p className={`transition-all duration-300 text-xl text-text-secondary leading-relaxed col-start-2 ${showAnswer ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0'}`}>
+        <PlusIcon className={`w-7 sm:w-6 h-[calc(1.25rem*1.625)] sm:h-[calc(1.125rem*1.625)] text-text-secondary transition-all ${showAnswer ? 'rotate-45' : 'rotate-0'}`} />
+        <h3 className="text-xl sm:text-lg leading-relaxed! my-auto">{question}</h3>
+        <p className={`transition-all duration-300 text-xl sm:text-lg text-text-secondary leading-relaxed! col-start-2 ${showAnswer ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0'}`}>
           <span className="block pt-4">{answer}</span>
         </p>
       </div>
