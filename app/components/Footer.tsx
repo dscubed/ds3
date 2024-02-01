8850b02376cd2075d3a5e87bcf70646d58827508
Michael Ren <michaelren0129@gmail.com>
2024-02-01 14:28:05 +0800

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,11 +3,11 @@ import * as Icon from 'react-bootstrap-icons'
 
 export default function Footer () {
   return (
-    <footer className="relative z-20 px-4 py-8 bg-background-secondary">
+    <footer className="relative z-20 px-5 py-8 bg-background-secondary">
       <div className="flex flex-col gap-10 max-w-screen-xl mx-auto">
-        <div className="flex sm:flex-col gap-4 sm:gap-5 justify-between text-text-secondary">
-          <p className="sm:text-center">© {(new Date).getFullYear()} D3S</p>
-          <div className="flex gap-8 sm:mx-auto">
+        <div className="flex md:flex-col gap-4 md:gap-5 justify-between text-text-secondary">
+          <p className="md:text-center">© {(new Date).getFullYear()} D3S · Data Science Student Society</p>
+          <div className="flex gap-8 md:mx-auto">
             <Link href="mailto:hello@dscubed.org.au" target="_blank">
               <Icon.EnvelopeFill className="w-5 h-5"/>
             </Link>
