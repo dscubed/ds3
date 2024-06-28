c18cbe09171036435ad62d5bb04a30c37d277c4b
Michael Ren <michaelren0129@gmail.com>
2024-06-28 17:02:44 +1000

    Add gallery image and update committee photo

--- app/components/home/galleryData.ts
+++ app/components/home/galleryData.ts
@@ -1,10 +1,20 @@
 import event1Image from '@/public/gallery/event1.jpg'
 import event2Image from '@/public/gallery/event2.jpg'
+import event3Image from '@/public/gallery/event3.jpg'
+import event4Image from '@/public/gallery/event4.jpg'
 import { StaticImageData } from 'next/image'
 
 const data: { src: StaticImageData, description: string }[] = [
   {
+    src: event3Image,
+    description: ''
+  },
+  {
     src: event1Image,
+    description: ''
+  },
+  {
+    src: event4Image,
     description: ''
   },
   {
